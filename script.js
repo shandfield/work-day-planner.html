13891a830ebf7876673994d158b2980e554263fa
Shelby <63683598+shandfield@users.noreply.github.com>
2020-07-05 16:15:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//need to call on current day id with a class of lead 
-$(".saveBtn").on("click",function(){//need to create a save button function that saves the information entered in
-})
-
-//need to create a way for the table to be colored to show past, present, and future times 
+function save(){
+    var todo= document.getElementById("todoInput").value;
+        localStorage.setItem("text", text);
+    }
