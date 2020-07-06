dc168685f1a77e0f39f6875d316b15210519c10c
Shelby <63683598+shandfield@users.noreply.github.com>
2020-07-06 17:28:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
        setInterval(() => {
         var currentTime = moment ().format("dddd, MMMM Do YYYY, h:mm:ss a");
        $("#currentDay").text((currentTime), 1000);
+       });
+       
+       setTimeout(()=>{
+       var timeblocks= moment.duration().asHours(9,10,11,12,13,14,15,16,17);
+       $("#timeblocks").text((timeblocks));
+       });
+
+
+
+
+
+
+
+
+
+});
